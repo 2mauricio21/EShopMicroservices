def16585b5c8b331205b87e4fc16d6c579306504
2mauricio21 <mauricio.salso@hotmail.com>
2026-03-24 09:18:42 -0300

    feat: Registrando as bibliotecas MediatR e Cartel  com injeção de dependencia
    - Injetando essas dependencias para o proprio projeto fazer o mapeamento dos minimalAPI com o Carter
    - configurando o Mediator para fazer o mapeamento das Classes direto no assembly

--- eshop-microservices/Service/Catalog/Catalog.APICatalog.API/Program.cs
+++ eshop-microservices/Service/Catalog/Catalog.APICatalog.API/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    // Register all MediatR handlers from the current assembly
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
