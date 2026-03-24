894487d1c1c7742ff16176d375665adbcaccb924
2mauricio21 <mauricio.salso@hotmail.com>
2026-03-24 09:13:11 -0300

    feat: Adicionada a função Mapster e refatorado o mapeamento do endpoint CreateProduct
    Integrado o Mapster para mapeamento de objetos e atualizado o endpoint CreateProductEndpoint para utilizar a interface ICarterModule. Refatorado o endpoint para mapear pedidos e respostas com o Mapster, retornando o código 201 Created com a localização do recurso. Adicionado diretivas globais using para Carter, Mapster e MediatR.

--- eshop-microservices/Service/Catalog/Catalog.APICatalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ eshop-microservices/Service/Catalog/Catalog.APICatalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,5 @@
-﻿namespace Catalog.APICatalog.API.Products.CreateProduct
+﻿
+namespace Catalog.APICatalog.API.Products.CreateProduct
 {
     public record CreateProductRequest(
         string Name, 
@@ -8,7 +9,26 @@
         decimal Price);
 
     public record CreateProductResponse(Guid Id);
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender Sender) =>
+            {
+                // faz o mapeamento do request para o comando usando o Mapster
+                var command = request.Adapt<CreateProductCommand>();
+
+                // envia o comando para o MediatR processar e aguarda o resultado
+                var result = await Sender.Send(command);
+                // mapeia o resultado para a resposta usando o Mapster
+                var response = result.Adapt<CreateProductResponse>();
+                // retorna a resposta com o status 201 Created e o local do recurso criado
+                return Results.Created($"/products/{result.Id}", response);
+            }).WithName("CreateProduct")
+              .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+              .ProducesProblem(StatusCodes.Status400BadRequest)
+              .WithSummary("Create Product")
+              .WithDescription("Create Product");
+        }
     }
 }
