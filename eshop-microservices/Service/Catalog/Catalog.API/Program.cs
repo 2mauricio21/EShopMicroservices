167ad70e15acf29732d280ade3040c7c8e4e35d0
2mauricio21 <mauricio.salso@hotmail.com>
2026-03-25 11:47:38 -0300

    feat: Configuração do postgress no docker compose.
    - varios arquivos foram modificados apenas por que renomeei a pasta

--- eshop-microservices/Service/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Service/Catalog/Catalog.API/Program.cs
@@ -8,6 +8,12 @@ builder.Services.AddMediatR(config =>
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
 
+builder.Services.AddMarten(options =>
+{
+    // Configure Marten to connect to the PostgreSQL database
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
