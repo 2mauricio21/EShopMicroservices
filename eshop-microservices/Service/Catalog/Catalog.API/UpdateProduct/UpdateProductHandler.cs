f24467702799f0989f5a5d9647631879fb49b0ce
2mauricio21 <mauricio.salso@hotmail.com>
2026-04-01 09:12:02 -0300

    feat: Add metodo Update padrão CQRS, Mediator e Carter

---
+++ eshop-microservices/Service/Catalog/Catalog.API/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Catalog.API.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSucess);
+
+
+    internal class UpdateProductHandlerHandler(IDocumentSession session, ILogger<UpdateProductHandlerHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductHandler.Handle called with {@Command}", command);
+
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+            if (product is null)
+                throw new ProductNotFountException();
+
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.Category = command.Category;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
