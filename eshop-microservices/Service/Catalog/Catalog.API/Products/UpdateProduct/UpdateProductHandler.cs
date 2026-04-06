0a209a0fa237d41d92ac3882465b3a26d0f67d05
2mauricio21 <mauricio.salso@hotmail.com>
2026-04-06 10:35:29 -0300

    feat: Adicionando Command validate com FluentValidate que será injetado automaticamente.

--- eshop-microservices/Service/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ eshop-microservices/Service/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -3,6 +3,20 @@
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSucess);
 
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required");
+
+            RuleFor(x => x.Name).NotEmpty()
+                .WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
+
 
     internal class UpdateProductHandlerHandler(IDocumentSession session, ILogger<UpdateProductHandlerHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
