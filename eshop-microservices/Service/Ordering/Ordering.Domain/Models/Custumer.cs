8cdf13d373ceedfe8cd08ee4df84ad600351b264
Mauricio Souza <mauricio.salso@hotmail.com>
2026-05-06 22:57:04 -0300

    feat: Implementando DDD para criar base Domain

---
+++ eshop-microservices/Service/Ordering/Ordering.Domain/Models/Custumer.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Custumer : Entity<CustumerId>
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+
+        public static Custumer Create(CustumerId id, string name, string email)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentException.ThrowIfNullOrEmpty(email);
+
+            var custumer = new Custumer
+            {
+                Id = id,
+                Name = name,
+                Email = email
+            };
+
+            return custumer;
+        }
+    }
+}
